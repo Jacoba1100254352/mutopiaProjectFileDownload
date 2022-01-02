a8173119e4a3d5436b8998f4e287f1fc52704658
Jacob Anderson <jacobdanderson@gmail.com>
2022-01-01 20:20:04 -0700

    Updates

--- downloadPDFs.py
+++ downloadPDFs.py
@@ -1,19 +1,23 @@
 from requests_html import HTMLSession
 import wget
 import os.path
+import time
 
 session = HTMLSession()
 
-url = f'https://www.mutopiaproject.org/ftp/'
+url = "https://www.mutopiaproject.org/ftp/"
 fileType = "let.pdf"
-r = session.get(url)
 toPrint = []
 
 # get all artists on the main page except the parent directory link
-artists = r.html.xpath("//td[not(contains(a, 'Parent Directory'))]/a")
+artists = session.get(url).html.xpath("//td[not(contains(a, 'Parent Directory'))]/a")
 artists = [url + a.attrs["href"] for a in artists]
-print(artists)
 
+# Used to update the user as to the current progress of the program, though not entirely necessary
+timeWaiting = time.time()
+
+# start sorting and digging until only the specified fileType are kept
+print("Sorting...")
 while len(artists) != 0:
 
     artistsCopy = artists
@@ -27,11 +31,8 @@ while len(artists) != 0:
                 artists.remove(artist)
                 continue
 
-            # get full working path
-            r = session.get(artist)
-
-            # get all sub-links besides the parent directory available
-            sublinks = r.html.xpath("//td[not(contains(a, 'Parent Directory'))]/a")
+            # get full working path and all sub-links besides the parent directory available
+            sublinks = session.get(artist).html.xpath("//td[not(contains(a, 'Parent Directory'))]/a")
 
             # add the remaining head links to the artists to sort and remove the current artist (replaced by its lower links [now visited])
             artists.remove(artist)
@@ -41,13 +42,16 @@ while len(artists) != 0:
         else:
             toPrint.append(artist)
             artists.remove(artist)
-    print(artists)
 
-print("\n\n\nTo Print:")
-print(toPrint)
+        # Used to update the user as to the current progress of the program, though not entirely necessary
+        if time.time() - timeWaiting > 60:
+            print("Still sorting...")
+            timeWaiting = time.time()
 
+# Download all of the collected links/files
+print("Downloading...")
 for file in toPrint:
     try:
         wget.download(file, f'/Users/download/path/{os.path.basename(file)}')
     except:
-        print("A problem occurred")
+        print("A problem occurred with file: " + str(file))
